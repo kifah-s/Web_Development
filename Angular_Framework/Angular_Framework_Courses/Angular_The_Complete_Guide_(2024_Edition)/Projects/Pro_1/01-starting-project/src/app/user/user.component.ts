142fd91e90b81c1fb6cc0ca1a14e78335ca3bd12
kifah saloum <kifah.saloum.96@gmail.com>
2024-09-13 18:32:10 +0300

    Add lesson 29: More Flexible Components.

--- Angular_Framework/Angular_Framework_Courses/Angular_The_Complete_Guide_(2024_Edition)/Projects/Pro_1/01-starting-project/src/app/user/user.component.ts
+++ Angular_Framework/Angular_Framework_Courses/Angular_The_Complete_Guide_(2024_Edition)/Projects/Pro_1/01-starting-project/src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, signal, computed } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,30 +8,6 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  //! State.
-  /* 
-  selectedUser = DUMMY_USERS[randomIndex];
-
-  //* Getter.
-  get imagePath() {
-    return '/assets/users/' + this.selectedUser.avatar;
-  }
-
   //* On Select User.
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
-  }
-  */
-
-  //! Signal.
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-
-  imagePath = computed(() => '/assets/users/' + this.selectedUser().avatar);
-
-  //* On Select User.
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-  }
+  onSelectUser() {}
 }
---
+++ Angular_Framework/Angular_Framework_Courses/Angular_The_Complete_Guide_(2024_Edition)/Sections/Sec_2_Angular_Essentials/Les_29_More_Flexible_Components/Lesson_Summary/Lesson_Summary.md
@@ -0,0 +1,3 @@
+## Lesson Summary:
+
+- We Need More Flexible Components.
