409a95f4ae90bb203bbdc8064b57d1ef815e3ee2
kifah_saloum <kifah.saloum.96@gmail.com>
2024-10-09 11:59:40 +0300

    Using Signal in user-input component

--- Angular_Framework/Angular_Framework_Courses/Angular_The_Complete_Guide_(2024_Edition)/Projects/Pro_2_Investment_Calculator/investment-calculator-v2/src/app/user-input/user-input.component.ts
+++ Angular_Framework/Angular_Framework_Courses/Angular_The_Complete_Guide_(2024_Edition)/Projects/Pro_2_Investment_Calculator/investment-calculator-v2/src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import type { InvestmentInput } from '../investment-input.model';
 
@@ -12,10 +12,10 @@ import type { InvestmentInput } from '../investment-input.model';
 export class UserInputComponent {
   @Output() calculate = new EventEmitter<InvestmentInput>();
 
-  enteredInitialInvestment = '0';
-  enteredAnnualInvestment = '0';
-  enteredExpectedReturn = '5';
-  enteredDuration = '10';
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
 
   onSubmit() {
     // console.log('Submitted');
@@ -25,10 +25,10 @@ export class UserInputComponent {
     // console.log(this.enteredDuration);
 
     this.calculate.emit({
-      initialInvestment: +this.enteredAnnualInvestment,
-      annualInvestment: +this.enteredAnnualInvestment,
-      expectedReturn: +this.enteredExpectedReturn,
-      duration: +this.enteredDuration,
+      initialInvestment: +this.enteredAnnualInvestment(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      duration: +this.enteredDuration(),
     });
   }
 }
